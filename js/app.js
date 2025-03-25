8227d5c07d541629ddec1d94f095922e21762c83
HMNaik23 <hetvinaik.mscit20@vnsgu.ac.in>
2025-03-25 16:03:57 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// main css
+import "../styles/app.scss";
+
+// functions js
+import "../vendor/js/layout.min.js";
+
+// magnific popup js
+import "../vendor/js/magnific.min.js";
+
+// nice select js
+import "../vendor/js/nice-select.min.js";
+
+// owl carousel js
+import "../vendor/js/owl.min.js";
+
+// counter up js
+import "../vendor/js/counter-up.min.js";
+
+// waypoint js
+import "../vendor/js/waypoint.min.js";
+
+// wow js
+import WOW from "wowjs";
+new WOW.WOW({
+  live: false,
+}).init();
+import "wowjs/css/libs/animate.css";
+
+import "./modules.js";
